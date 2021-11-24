7208554900a7373efd488e842b0a274dbf3fec58
Emma Ramstedt <71063351+Deliciaes@users.noreply.github.com>
2021-11-24 16:51:39 +0100

    interior color picker

--- style.js
+++ style.js
@@ -1,22 +1,44 @@
-const bikeColor = document.getElementById('bike-img');
-const red = document.querySelector('.button-exterior-black');
-const orange = document.querySelector('.button-exterior-white');
-const pink = document.querySelector('.button-exterior-orange');
+
+const bikeExtColor = document.getElementById('bike-exterior-img');
+const blackExt = document.querySelector('.button-exterior-black');
+const whiteExt = document.querySelector('.button-exterior-white');
+const orangeExt = document.querySelector('.button-exterior-orange');
+
+const bikeIntColor = document.getElementById('bike-interior-img');
+const blackInt = document.querySelector('.button-interior-black');
+const whiteInt = document.querySelector('.button-interior-white');
+const orangeInt = document.querySelector('.button-interior-orange');
 
 
-red.addEventListener('click', () => {
-        bikeColor.alt = "Photo of the Smoove bike exterior in Black";
-        bikeColor.src = "images/exterior_black.png";
+blackExt.addEventListener('click', () => {
+    bikeExtColor.alt = "Photo of the Smoove bike exterior in black";
+    bikeExtColor.src = "images/exterior_black.png";
+});
 
-})
+whiteExt.addEventListener('click', () => {
+    bikeExtColor.alt = "Photo of the Smoove bike exterior in white";
+    bikeExtColor.src = "images/exterior_white.png";
+});
 
-orange.addEventListener('click', () => {
-    bikeColor.alt = "Photo of the Smoove bike exterior in white";
-    bikeColor.src = "images/exterior_white.png";
-})
+orangeExt.addEventListener('click', () => {
+    bikeExtColor.alt = "Photo of the Smoove bike exterior in orange";
+    bikeExtColor.src = "images/exterior_orange.png";
+});
 
-pink.addEventListener('click', () => {
-    bikeColor.alt = "Photo of the Smoove bike exterior in orange";
-    bikeColor.src = "images/exterior_orange.png";
-})
-   
+
+
+blackInt.addEventListener('click', () => {
+    bikeIntColor.alt = "Photo of the Smoove bike interior in black";
+    bikeIntColor.src = "images/interior_black.png";
+});
+
+
+whiteInt.addEventListener('click', () => {
+    bikeIntColor.alt = "Photo of the Smoove bike interior in white";
+    bikeIntColor.src = "images/interior_white.png";
+});
+
+orangeInt.addEventListener('click', () => {
+    bikeIntColor.alt = "Photo of the Smoove bike interior in orange";
+    bikeIntColor.src = "images/interior_orange.png";
+});
