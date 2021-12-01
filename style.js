2afe48c8520284b7b208ae761475e7d529896a04
Emma Ramstedt <71063351+Deliciaes@users.noreply.github.com>
2021-12-01 21:19:03 +0100

    added green border buttons
    color picker buttons are now green

--- style.js
+++ style.js
@@ -14,6 +14,10 @@ blackExt.addEventListener('click', () => {
 
     bikeExtColor.alt = "Photo of the Smoove bike exterior in black";
     bikeExtColor.src = "images/color-black.jpg";
+    blackExt.style.border = "solid";
+    blackExt.style.borderColor = "#65da8c";
+    whiteExt.style.border = "none";
+    orangeExt.style.border = "none";
 });
 
 
@@ -21,12 +25,20 @@ whiteExt.addEventListener('click', () => {
 
     bikeExtColor.alt = "Photo of the Smoove bike exterior in white";
     bikeExtColor.src = "images/color-white.jpg";
+    whiteExt.style.border = "solid";
+    whiteExt.style.borderColor = "#65da8c";
+    blackExt.style.border = "none";
+    orangeExt.style.border = "none";
     });
 
 orangeExt.addEventListener('click', () => {
 
     bikeExtColor.alt = "Photo of the Smoove bike exterior in orange";
     bikeExtColor.src = "images/color-orange.jpg";
+    orangeExt.style.border = "solid";
+    orangeExt.style.borderColor = "#65da8c";
+    whiteExt.style.border = "none";
+    blackExt.style.border = "none";
   });
 
 
@@ -35,6 +47,10 @@ blackInt.addEventListener('click', () => {
 
     bikeIntColor.alt = "Photo of the Smoove bike interior in black";
     bikeIntColor.src = "images/interior-black.jpg";
+    blackInt.style.border = "solid";
+    blackInt.style.borderColor = "#65da8c";
+    whiteInt.style.border = "none";
+    orangeInt.style.border = "none";
 });
 
 
@@ -42,12 +58,20 @@ whiteInt.addEventListener('click', () => {
 
     bikeIntColor.alt = "Photo of the Smoove bike interior in white";
     bikeIntColor.src = "images/interior-white.jpg";
+    whiteInt.style.border = "solid";
+    whiteInt.style.borderColor = "#65da8c";
+    blackInt.style.border = "none";
+    orangeInt.style.border = "none";
 });
 
 orangeInt.addEventListener('click', () => {
 
     bikeIntColor.alt = "Photo of the Smoove bike interior in orange";
     bikeIntColor.src = "images/interior-orange.jpg";
+    orangeInt.style.border = "solid";
+    orangeInt.style.borderColor = "#65da8c";
+    whiteInt.style.border = "none";
+    blackInt.style.border = "none";
 });
 
 
